3596e083c5cc65d5ca4ae5c61242c83450e0cfc5
enan22pt <117173673+enan22pt@users.noreply.github.com>
2023-04-06 15:30:18 +0200

    Adding actions to the to do list

--- .github/todo.js
+++ .github/todo.js
@@ -1,13 +1,21 @@
 const addButton = document.getElementById("addbutton");
 const jumpingCat = document.getElementById("buttoncat");
-
+const toDoListElement = document.getElementById("actuallist");
+const addToListElement = document.getElementById("entertext");
 jumpingCat.style.visibility = "hidden";
 
-addButton.addEventListener("click", () =>{
-    jumpingCat.style.visibility = "visible";
+addButton.addEventListener("click", addToDoList);
 
+function addToDoList(event){
+//button cat    
+    jumpingCat.style.visibility = "visible";
 setTimeout(() =>{
     jumpingCat.style.visibility = "hidden";
 }, 1000);
 
-});
+// adding actions to the list
+const addedToList = document.createElement("li");    
+addedToList.innerText = addToListElement.value;
+toDoListElement.appendChild(addedToList);
+addedToList.value="";
+}
