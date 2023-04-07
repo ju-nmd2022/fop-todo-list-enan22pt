1bce7051de047f58bdccf319e1ec476f39a00ea6
enan22pt <117173673+enan22pt@users.noreply.github.com>
2023-04-07 22:41:52 +0200

    marking actions as done

--- .github/todo.js
+++ .github/todo.js
@@ -3,7 +3,7 @@ const jumpingCat = document.getElementById("buttoncat");
 const toDoListElement = document.getElementById("actuallist");
 const addToListElement = document.getElementById("entertext");
 jumpingCat.style.visibility = "hidden";
-
+const toDoCat = document.createElement("img");
 addButton.addEventListener("click", addToDoList);
 
 function addToDoList(event){
@@ -19,9 +19,33 @@ addedToList.innerText = addToListElement.value;
 toDoListElement.appendChild(addedToList);
 addedToList.value="";
 
+//hover cat for the to do list
+
+toDoCat.src = "cat2.png";
+toDoCat.classList.add("toDoCat");
+addedToList.appendChild(toDoCat);
+toDoListElement.appendChild(addedToList);
+
 //clear the imput value
 if(addToListElement.value!=""){
     addToListElement.value="";
 }
+addedToList.addEventListener("click", markAsDone);
+
+}
+
+const doneListElement= document.getElementById("donelist");
+
+// mark actions as done
+function markAsDone(event) {
+    const doneAction = event.target;
+  doneAction.classList.toggle("done");
+  if (doneAction.classList.contains("done")) {
+    const doneCat = doneAction.querySelector('.toDoCat');
+    doneListElement.appendChild(doneAction);
+    doneListElement.removeChild(doneCat);
+  } else {
+    toDoListElement.appendChild(doneAction);
+  }
 }
 
