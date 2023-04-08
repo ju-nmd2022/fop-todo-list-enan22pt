ce91bda1d7cf2bdf7c863cbc8db11c0d7637e3ba
enan22pt <117173673+enan22pt@users.noreply.github.com>
2023-04-08 23:47:52 +0200

    delete button

--- .github/todo.js
+++ .github/todo.js
@@ -2,8 +2,10 @@ const addButton = document.getElementById("addbutton");
 const jumpingCat = document.getElementById("buttoncat");
 const toDoListElement = document.getElementById("actuallist");
 const addToListElement = document.getElementById("entertext");
+const doneListElement= document.getElementById("donelist");
+
 jumpingCat.style.visibility = "hidden";
-const toDoCat = document.createElement("img");
+
 addButton.addEventListener("click", addToDoList);
 
 function addToDoList(event){
@@ -14,38 +16,40 @@ setTimeout(() =>{
 }, 1000);
 
 // adding actions to the list
-const addedToList = document.createElement("li");    
+const addedToList = document.createElement("li");   
 addedToList.innerText = addToListElement.value;
 toDoListElement.appendChild(addedToList);
 addedToList.value="";
 
-//hover cat for the to do list
-
-toDoCat.src = "cat2.png";
-toDoCat.classList.add("toDoCat");
-addedToList.appendChild(toDoCat);
-toDoListElement.appendChild(addedToList);
+//delete action button
+const deleteButton = document.createElement("button");
+deleteButton.innerText= "✖︎";
+addedToList.appendChild(deleteButton);
+deleteButton.classList.add("deletebutton");
+deleteButton.addEventListener("click", deleteAction);
 
 //clear the imput value
 if(addToListElement.value!=""){
     addToListElement.value="";
 }
 addedToList.addEventListener("click", markAsDone);
-
 }
-
-const doneListElement= document.getElementById("donelist");
 
 // mark actions as done
 function markAsDone(event) {
     const doneAction = event.target;
   doneAction.classList.toggle("done");
   if (doneAction.classList.contains("done")) {
-    const doneCat = doneAction.querySelector('.toDoCat');
     doneListElement.appendChild(doneAction);
-    doneListElement.removeChild(doneCat);
   } else {
     toDoListElement.appendChild(doneAction);
   }
 }
 
+//delete actions from both lists 
+//this code is inspired from Chat GPT (my initial code was working just on the toDoList, but not doneList)
+function deleteAction(event){
+  const actionElement = event.target.parentElement;
+  const parentElement = actionElement.parentElement;
+  parentElement.removeChild(actionElement);
+}
